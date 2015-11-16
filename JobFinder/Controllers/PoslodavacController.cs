d1f98ebf62784a96738c3ef0854c4d3b95f7a016
emujic1 <emujic1@etf.unsa.ba>
2015-11-16 16:49:02 +0100

    promjene

--- JobFinder/Controllers/PoslodavacController.cs
+++ JobFinder/Controllers/PoslodavacController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JobFinder.Models;
 
 namespace JobFinder.Controllers
 {
@@ -13,7 +14,10 @@ namespace JobFinder.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            OglasiKategorijeModel okm = new OglasiKategorijeModel(); 
+            bazaEntities dc = new bazaEntities();
+            okm.Kategorije = dc.kategorije.ToArray();
+            return View("Index",okm);
         }
 
     }
